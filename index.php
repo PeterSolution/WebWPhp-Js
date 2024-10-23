7cb15bed0739243bffa04e30da1b51b65346936f
PeterSolution <piotrwolff@op.pl>
2024-10-23 06:51:56 +0200

    first

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>CV Piotr Wolff</title>
+	<link rel="stylesheet" href="cssstrona.css">
+</head>
+<body>
+    <div class="container">
+        <img src="image.png" alt="moj obrazek" class="profile-pic">
+        <center><h1>Piotr Wolff</h1></center>
+        <p>Email: PiotrWolff@op.pl | Telefon: 123 456 789</p>
+        
+        <h2>Podsumowanie zawodowe</h2>
+        <p>Doświadczony specjalista IT z 2-letnim stażem w tworzeniu aplikacji w technologii dotnet i zarządzaniu projektami.</p>
+        
+        <h2>Doświadczenie zawodowe</h2>
+        <h3>Programista, firma xxx</h3>
+        <ul>
+            <li>Kierowałem 3 osobowym zespołem mającym dla aplikacji w czasie covida do ruszania się</li>
+            <li>Zajmowałem się implementacją nowych funkcjonalności i layoutem aplikacji aplikacji</li>
+        </ul>
+        
+        <h3>Informatyk, w zakładzie produkcyjnym</h3>
+        <ul>
+            <li>Uczestniczyłem w tworzeniu aplikacji do raportowania zdarzeń na produkcji</li>
+            <li>Współpracowałem w tworzeniu i modernizacji strony internetowej firmy</li>
+        </ul>
+        
+        <h2>Wykształcenie</h2>
+        <p>Technik informatyk, obecny student na Uniwersytecie UKW</p>
+        
+        <h2>Umiejętności</h2>
+        <ul>
+            <li>Języki programowania: C#, Python, Java</li>
+            <li>Frameworki: React, Mapper, EntityFramework, Pyautogui, Fresk</li>
+            <li>Bazy danych: SQL managment studio, Oracle</li>
+            <li>Narzędzia: Git, Docker</li>
+			<li>Certyfikaty: EE09, EE08</li>
+			<li>Ukończone kursy: Rest API, Tester oprogramowania</li>
+        </ul>
+		<h2>O mnie</h2>
+		
+		
+    </div>
+</body>
+</html>
